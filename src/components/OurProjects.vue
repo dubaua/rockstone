e43f53ea8cc31940cb792c4b6a0b7ebe52b51511
dubaua <dubaua@gmail.com>
2018-10-06 19:44:13 +0500

    preload projectcovers

--- src/components/OurProjects.vue
+++ src/components/OurProjects.vue
@@ -1,36 +1,39 @@
 <template lang="pug">
   section.section.our-projects
+    .our-projects__cover-placeholder(
+      v-for="(project, index) in content.our_projects"
+      :key="project._id"
+      :style="getCover(index).style",
+    )
     .section__container
-      transition-sequence(v-bind="getTransitionConfig(1, 5, 'ourProjects')" @transitionAnimated="showNext('ourProjects')") 
+      transition-sequence(v-bind="getTransitionConfig(0, 4, 'ourProjects')" @transitionAnimated="showNext('ourProjects')") 
         h1.subtitle.title--display.our-projects__title {{ content.common.our_projects_title }}
-      transition-sequence(v-bind="getTransitionConfig(4, 5, 'ourProjects')" @transitionAnimated="showNext('ourProjects')" :isBlocking="!isSwiperReady") 
+      transition-sequence(v-bind="getTransitionConfig(3, 4, 'ourProjects')" @transitionAnimated="showNext('ourProjects')" :isBlocking="!isSwiperReady") 
         .our-projects__pagination(v-if="isSwiperReady")
           pagination(:config="paginationConfig")
-      transition-sequence(v-bind="getTransitionConfig(5, 5, 'ourProjects')" @transitionAnimated="showNext('ourProjects')")
+      transition-sequence(v-bind="getTransitionConfig(4, 4, 'ourProjects')" @transitionAnimated="showNext('ourProjects')")
         mq-layout(
           v-if="isSwiperReady",
           mq="lg+",
-          :style="coverStyle",
-          :class="coverClass").our-projects__cover
+          :style="getCover(currentProjectIndex).style",
+          :class="getCover(currentProjectIndex).className").our-projects__cover
       .our-projects__slider(v-if="content.our_projects")
-        transition-sequence(v-bind="getTransitionConfig(2, 5, 'ourProjects')" @transitionAnimated="showNext('ourProjects')") 
+        transition-sequence(v-bind="getTransitionConfig(1, 4, 'ourProjects')" @transitionAnimated="showNext('ourProjects')") 
           mq-layout(mq="lg+").our-projects__dotted
             .dots
-        transition-sequence(v-bind="getTransitionConfig(3, 5, 'ourProjects')" @transitionAnimated="showNext('ourProjects')") 
+        transition-sequence(v-bind="getTransitionConfig(2, 4, 'ourProjects')" @transitionAnimated="showNext('ourProjects')") 
           swiper(:options="ourProjectsSwiperOptions", ref="ourProjectsSwiper", @ready="onSwiperInit('ourProjects')" @slideChangeTransitionEnd="onSlideChangeEnd")
             swiper-slide(
               v-for="(project, index) in content.our_projects"
               :key="project._id"
               )
               our-projects-item(:project="project", :readmore="content.common.readmore", :id="index")
-        transition-sequence(v-bind="getTransitionConfig(2, 5, 'ourProjects')") 
-          button.js-our-projects-swiper-prev.slider-button.slider-button--round.slider-button.our-projects__prev
-            icon(glyph="arrow-left--square", :width="24", :height="24").slider-button__glyph
-        transition-sequence(v-bind="getTransitionConfig(2, 5, 'ourProjects')") 
-          button.js-our-projects-swiper-next.slider-button.slider-button--round.slider-button.our-projects__next
-            icon(glyph="arrow-right--square", :width="24", :height="24").slider-button__glyph
+        button.js-our-projects-swiper-prev.slider-button.slider-button--round.slider-button.our-projects__prev
+          icon(glyph="arrow-left--square", :width="24", :height="24").slider-button__glyph
+        button.js-our-projects-swiper-next.slider-button.slider-button--round.slider-button.our-projects__next
+          icon(glyph="arrow-right--square", :width="24", :height="24").slider-button__glyph
     parallax-scene.section__mountain.section__mountain--4-1.section__mountain--d-n
-      transition-sequence(v-bind="getTransitionConfig(0, 5, 'ourProjects')" @transitionAnimated="showNext('ourProjects')") 
+      transition-sequence(v-bind="getTransitionConfig(0, 4, 'ourProjects')" @transitionAnimated="showNext('ourProjects')") 
         icon(glyph="mountain-group-4-1")
     mq-layout(mq="lg+").scrolldown.scrolldown--above.scrolldown--accent
       .scrolldown__text
@@ -81,20 +84,6 @@ export default {
     currentProjectIndex() {
       return this.$store.state.sections.ourProjects.currentProjectIndex;
     },
-    currentProject() {
-      return this.content.our_projects[this.currentProjectIndex];
-    },
-    coverUrl() {
-      return this.currentProject.cover 
-        ? this.currentProject.cover.path
-        : null;
-    },
-    coverStyle() {
-      return this.coverUrl ? `background-image: url(${this.coverUrl})` : null;
-    },
-    coverClass() {
-      return this.currentProject.cover_align ? `our-projects__cover--${this.currentProject.cover_align}` : null
-    },
     fromLg() {
       return this.$mq === 'lg' || this.$mq === 'xl' || this.$mq === 'xxl';
     },
@@ -103,6 +92,14 @@ export default {
     },
   },
   methods: {
+    getCover(index) {
+      const url = this.content.our_projects[index].cover ? this.content.our_projects[index].cover.path : null;
+      const align = this.content.our_projects[index].cover_align;
+      return {
+        style: `background-image: url(${url})`,
+        className: `our-projects__cover--${align}`,
+      };
+    },
     showNext(key) {
       this.$store.commit('nextStep', { key })
     },
@@ -190,6 +187,10 @@ export default {
     @include breakpoint("lg") {
       top: 16px;
     }
+  }
+  &__cover-placeholder {
+    width: 0;
+    height: 0;
   }
 }
 </style>
