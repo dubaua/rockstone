07f07d3a900c5cabac976e42f2fad2fefb8bf1e4
dubaua <dubaua@gmail.com>
2018-10-13 00:01:10 +0500

    also register js components, skip self

--- src/components/globals/index.js
+++ src/components/globals/index.js
@@ -2,17 +2,26 @@ import Vue from "vue";
 import upperFirst from "lodash/upperFirst";
 import camelCase from "lodash/camelCase";
 
+const STARTS_WITH_DOT_SLASH_REGEXP = /^\.\//;
+const INDEX_DOT_ANYTHING_REGEXP = /(\/index)?\.\w+$/;
+
 // Require in a base component context, include subdirectories = true
-// TODO don't register global index.js as component
 const requireComponent = require.context(".", true, /\.(vue|js)$/);
 
 requireComponent.keys().forEach(fileName => {
+  // Don't register this file
+  if (fileName === "./index.js") return;
+
   // Get component config
   const componentConfig = requireComponent(fileName);
 
   // Get PascalCase name of component
   const componentName = upperFirst(
-    camelCase(fileName.replace(/^\.\//, "").replace(/(index)?\.\w+$/, ""))
+    camelCase(
+      fileName
+        .replace(STARTS_WITH_DOT_SLASH_REGEXP, "")
+        .replace(INDEX_DOT_ANYTHING_REGEXP, "")
+    )
   );
 
   // Register component globally
