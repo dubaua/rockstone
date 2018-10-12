3dea2c97dee7571aee88ee3d4c93d50aaec021e6
dubaua <dubaua@gmail.com>
2018-10-12 13:06:18 +0500

    remove Y parallax on who we are

--- src/components/WhoWeAre.vue
+++ src/components/WhoWeAre.vue
@@ -10,16 +10,16 @@
         .spread-block__col.spread-block__col--middle.who-we-are__content
           transition-sequence(v-bind="getTransitionConfig(3, 6, 'whoWeAre')" @transitionAnimated="showNext('whoWeAre')")
             .typographic(v-html="content.common.who_we_are_block_2")
-          parallax-scene(offset-y="1px").section__mountain.section__mountain--2-2.section__mountain--l-n
+          .section__mountain.section__mountain--2-2.section__mountain--l-n
             transition-sequence(v-bind="getTransitionConfig(4, 6, 'whoWeAre')" @transitionAnimated="showNext('whoWeAre')")
               icon(glyph="mountain-group-2-2")
         .spread-block__col.spread-block__col--right.who-we-are__content
           transition-sequence(v-bind="getTransitionConfig(5, 6, 'whoWeAre')" @transitionAnimated="showNext('whoWeAre')")
             .typographic(v-html="content.common.who_we_are_block_3")
-          parallax-scene(offset-y="1px").section__mountain.section__mountain--2-3.section__mountain--l-n
+          .section__mountain.section__mountain--2-3.section__mountain--l-n
             transition-sequence(v-bind="getTransitionConfig(6, 6, 'whoWeAre')" @transitionAnimated="showNext('whoWeAre')")
               icon(glyph="mountain-group-2-3")
-    parallax-scene(offset-y="1px").section__mountain.section__mountain--2-1.section__mountain--d-n
+    .section__mountain.section__mountain--2-1.section__mountain--d-n
       transition-sequence(v-bind="getTransitionConfig(0, 6, 'whoWeAre')" @transitionAnimated="showNext('whoWeAre')")
         icon(glyph="mountain-group-2-1")
 </template>
